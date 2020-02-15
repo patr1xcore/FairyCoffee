5da2e8b71c1a0b8ad2f3dff551e226fd1bbb8e28
patr1xcore <60846823+patr1xcore@users.noreply.github.com>
2020-02-15 17:03:41 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// MENU LINKS - SCROLL ANIMATION
+
+$(document).ready(function() {
+    var menu = $('.header_navigation, .header_content'),
+
+        menuLinks = menu.find('a');
+
+    menuLinks.on('click', function(event) {
+        $('html,body').animate({scrollTop: $(this.hash).offset().top}, 500);
+        event.preventDefault();
+    });
+});
+
+// HIDDEN NAVIGATION
+
+$(document).ready(function() {
+    // HIDE
+    $('.header_navigation-hidden').hide();
+    // TOGGLE
+    $('.header_hamburger-hidden').click(function() {
+        $('.header_navigation-hidden').fadeToggle(); 
+    });
+});
+
+// REVIEWS SECTION - CAROUSEL ANIMATION 
+
+$(document).ready(function(){
+  $('.autoplay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnHover: false,
+    arrows: false
+  });
+});
+
+// BACK TO TOP - ARROW
+
+$(document).ready(function() {
+
+    var backToTop = $('<a>', {
+        href: '#top',
+        class: 'back-to-top',
+        html: '<i class="fa fa-caret-up"></i>'
+    });
+
+    backToTop
+        .hide()
+        .appendTo('body')
+        .on('click', function() {
+            $('html,body').animate({scrollTop: 0});
+        });
+
+    var win = $(window);
+    win.on('scroll', function() {
+        if (win.scrollTop() >= 875) backToTop.fadeIn();
+        else backToTop.hide();
+    }); 
+});
